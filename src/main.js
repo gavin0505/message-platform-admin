2031e59de8b1c1b9344bba86d47c0ac544092db8
Gavin Zhang <zxb_worky@163.com>
2023-01-11 12:29:47 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI, {Message} from 'element-ui';
+import '@/assets/global.css';
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+Vue.prototype.$axios = axios;
+Vue.prototype.$message = Message;
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-button type="danger">home</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+
